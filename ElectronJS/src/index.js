9ce6a72cb30611320b05c2f015e9ee029138d498
Sub Estacion <subestacion@paseolagaleria.com>
2020-02-28 07:26:03 -0300

    Se agrega a repositorio

---
+++ ElectronJS/src/index.js
@@ -0,0 +1,133 @@
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+//Importamos el modulo url de node para manejo de url's
+const url = require('url');
+const path = require('path');
+
+//Preguntamos si estamos en desarrollo o produccion
+if(process.env.NODE_ENV !== 'production') {
+    /* Configuramos electron-reload solo si estamos en desarrollo */
+    require('electron-reload')(__dirname, {
+        //Quiero que reinicies cuando se haya modificado cuando se haya registrado en otro usuario
+        electron:path.join(__dirname, '../node_modules', '.bin', 'electretron')
+    });
+
+}
+
+let mainWindow;
+let ventana2, modalWindow;
+app.on("ready", ()=> {
+    //Una vez  que carga creamos una nueva ventana
+    mainWindow =  new BrowserWindow({});
+
+    //Cargamos el archivo index.html a la ventana mainWindow
+    mainWindow.loadURL(url.format({
+        //Donde esta el archivo, que le indicamos con path.join, donde __dirname seria la carpeta local 
+        pathname: path.join(__dirname, 'views/index.html'),
+        //le decimos a traves de que protocolo lo va a cargar
+        protocol: 'file',
+        slashes: true
+    }))
+
+    //Creamos un menu a partir de un arreglo
+    const mainMenu = Menu.buildFromTemplate(templateMenu);
+    //Integramos el el menu  a la ventana creada
+    Menu.setApplicationMenu(mainMenu);
+
+});
+
+function crearVentana2() {
+    ventana2 = new BrowserWindow({
+        width:400,
+        height: 330,
+        title: 'Ventana 2',
+        backgroundColor: '#008',
+        resizable: false
+    });
+
+    ventana2.loadURL(url.format({
+        pathname: path.join(__dirname, 'views/ventana2.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    ventana2.on('closed', ()=> {
+        ventana2 = null;
+    });
+}
+
+
+function modal() {
+    modalWindow = new BrowserWindow({
+        width:200,
+        height: 130,
+        title: 'Modal',
+        backgroundColor: '#023s',
+        parent: mainWindow,
+        modal: true,
+        show: false
+    });
+
+    modalWindow.once('ready-to-show', ()=> {
+        modalWindow.show()
+    });
+
+    modalWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'views/modal.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    modalWindow.on('closed', ()=> {
+        modalWindow = null;
+    });
+}
+
+
+
+const templateMenu = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'New Product',
+                accelerator: 'Ctrl+N',
+                click() {
+                    alert('New Product')
+                }
+            },
+
+            {
+                label: 'Eliminar todos los productos',
+                click() {
+
+                }
+            }
+        ]
+    }, 
+
+    {
+        label: 'Ventana 1',
+        click() {
+            crearVentana2();
+        }
+    },
+
+    {
+        label: 'Ventana 2'
+    },
+
+    {
+        label: 'Ventana 3'
+    },
+
+    {
+        label: 'Ventana 4'
+    }, 
+
+    {
+        label: 'Ventana 5'
+    }
+];
+
+
+//Funcion de las diferentes ventanas
